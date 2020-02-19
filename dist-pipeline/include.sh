0f8918477cdbb5d7661ae0a995a3ece9806f427b
Kyle McCormick <kmccormick@edx.org>
2020-02-19 15:58:26 -0500

    Fail in scripts if FRONTEND_NAME is not set.

--- dist-pipeline/include.sh
+++ dist-pipeline/include.sh
@@ -8,4 +8,11 @@ MSG='\033[0;32m'  # Green
 ERR='\033[0;31m'  # Red
 NC='\033[0m'      # No Color
 
-echo -e "\033[1;32mRunning '$0' for FRONTEND_NAME=$FRONTEND_NAME"
+msgbold='\033[1;32m'  # Green
+
+if [[ -z $frontend_name ]]; then
+	echo -e "${ERR}Environtment variable FRONTEND_NAME must be set. Exiting.${NC}"
+	exit 123
+else
+	echo -e "${msgbold}Running '$0' for FRONTEND_NAME=$frontend_name${NC}"
+fi
