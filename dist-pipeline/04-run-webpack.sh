c13e02404d7c3c71fe4c49abdf5dc65773449fdd
Kyle McCormick <kmccormick@edx.org>
2020-02-19 17:24:49 -0500

    Trailing slash in run-webpack; update todo

--- dist-pipeline/04-run-webpack.sh
+++ dist-pipeline/04-run-webpack.sh
@@ -4,6 +4,6 @@
 repo_dir=repos/frontend-app-${frontend_name}
 cd "$repo_dir"
 
-webpack_command="$(npm bin)/webpack --config 'webpack.prod.config.js' --output-public-path '/${frontend_name}'"
+webpack_command="$(npm bin)/webpack --config 'webpack.prod.config.js' --output-public-path '/${frontend_name}/'"
 echo -e "${MSG}Running webpack: ${webpack_command}${NC}"
 $webpack_command
