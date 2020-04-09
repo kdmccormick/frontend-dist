ae5a2be9952a4f31ed23a82be198678cc67edbe1
Kyle McCormick <kmccormick@edx.org>
2020-04-09 10:55:29 -0400

    Fix variable in check script

--- check.sh
+++ check.sh
@@ -5,6 +5,6 @@ SUCCESS='\033[0;32m'  # Green
 NC='\033[0m'          # No Color
 
 set -x
-curl --fail http://localhost:"$NGINX_HOST_PORT"/"$frontend_name"/ 1>/dev/null
+curl --fail http://localhost:"$DOCKER_HOST_PORT"/"$frontend_name"/ 1>/dev/null
 set +x
 echo -e "${SUCCESS}HTTP 2XX from index of ${frontend_name}; check complete.${NC}"
